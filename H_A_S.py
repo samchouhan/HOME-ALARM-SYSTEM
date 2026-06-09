b820abe7a5830dc9ab8a53f61332fb14dead408c
SAKSHAM_CHOUHAN <131700345+samchouhan@users.noreply.github.com>
2026-06-09 23:51:34 +0530

    Remove unused imports and variables in H_A_S.py

--- H_A_S.py
+++ H_A_S.py
@@ -1,6 +1,5 @@
 #Software system design code for the Home Alarm System (H_A_S)
-from plyer import notification
-alarm_armed = False
+
 
 if motion_detected and alarm_armed:
     print("INTRUDER DETECTED!")
